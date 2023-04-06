1e8c26d36112a4a7c48829d42bb0ed5191940663
王玉东 <1174872613@qq.com>
2023-04-06 17:44:32 +0800

    登录

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,27 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/voucher/**",
+                        "/user/code",
+                        "/user/login");
+
+    }
+}
