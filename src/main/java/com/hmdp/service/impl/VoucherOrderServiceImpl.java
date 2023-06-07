22849fe242d5afce82b8ef133e8f9f3729c589a4
Stone-WYD <1174872613@qq.com>
2023-06-07 12:28:04 +0800

    优惠券秒杀

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IVoucherService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +24,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Override
+    public Result seckillVocher(Long voucherId) {
+        // 1. 查询优惠券信息
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+        // 2. 查询优惠券是否过期或者未到时间
+        if (LocalDateTime.now().isBefore(seckillVoucher.getBeginTime())) {
+            return Result.fail("未到抢购时间！");
+        }
+        if (LocalDateTime.now().isAfter(seckillVoucher.getEndTime())){
+            return Result.fail("已经过了抢购时间！");
+        }
+        // 3. 判断库存是否充足
+
+
+        return null;
+    }
 }
