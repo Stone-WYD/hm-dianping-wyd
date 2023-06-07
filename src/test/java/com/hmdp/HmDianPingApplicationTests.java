72297cbf0b3a93bec945db49432bf6544a3676e1
Stone-WYD <1174872613@qq.com>
2023-06-07 12:29:12 +0800

    RedisId生成器测试代码

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,61 @@
 package com.hmdp;
 
+import com.hmdp.service.IShopService;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    @Resource
+    private RedisTemplate stringRedisTemplate;
+
+    @Resource
+    private IShopService iShopService;
+
+    @Resource
+    private CacheClient cacheClient;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void saveDataToRedis(){
+        cacheClient.setWithLogicExpire(CACHE_SHOP_KEY + 1, iShopService.getById(1), 30L, TimeUnit.SECONDS);
+    }
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 
 }
