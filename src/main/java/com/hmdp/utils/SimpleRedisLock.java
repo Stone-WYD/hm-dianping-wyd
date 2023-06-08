3626d10d6e8e363de471bfa7c988d1e40ddd544d
Stone-WYD <1174872613@qq.com>
2023-06-08 18:29:14 +0800

    redis 分布式锁使用lua使解锁具有原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,10 @@
 package com.hmdp.utils;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
-import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -20,7 +21,15 @@ public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
 
-    private static final String ID_PREFIX = UUID.randomUUID().toString() + "-";
+    private static final String ID_PREFIX = UUID.randomUUID() + "-";
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
 
     @Override
     public boolean tryLock(long timeoutSec) {
@@ -35,13 +44,22 @@ public class SimpleRedisLock implements ILock{
     @Override
     public void unlock() {
 
-        String value = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+/*        String key = KEY_PREFIX + name;
+        String value = stringRedisTemplate.opsForValue().get(key);
         // 获取线程标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 删除锁
         if (threadId.equals(value)){
-            stringRedisTemplate.delete(threadId);
-        }
-
+            // 删除锁
+            stringRedisTemplate.delete(key);
+        }*/
+        /*
+        * 上面的操作中，比较线程标识和删除锁是两个动作，并不是原子的，所以可能会引起并发问题
+        * 可以使用 lua 将操作合并起来让 redis 执行以达到原子性的需求
+        * */
+        // 调用 lua 脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
 }
